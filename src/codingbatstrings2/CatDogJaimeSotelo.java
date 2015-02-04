39a4ad92e593741509b8cf5105329f7792a1b418
botas1 <d.botas@hotmail.com>
2015-02-04 11:48:04 +0100

    Casi terminado

--- src/codingbatstrings2/CatDogJaimeSotelo.java
+++ src/codingbatstrings2/CatDogJaimeSotelo.java
@@ -3,14 +3,16 @@ package codingbatstrings2;
  * Autor: Sote
  */
 public class CatDogJaimeSotelo {
-    public boolean catDog (String str) {
-    int count = 0;
-  for (int i=0; i<str.length(); i++) {
-    if (str.charAt(i) == 'e') count++;
-     }
-  if (count == 2){
-   return true;
-    }
-  return false;
-    } 
-}
+   public boolean catDog (String str) {
+    int cat = 0;
+    int dogs= 0;
+  for (int i=0; i<str.length()-2; i++) {
+   if (str.substring(i, i+3).equals("cat"));
+            cat += 1;
+   if (str.substring(i, i+3).equals("dog"))
+            dogs += 1;
+                }
+   return cat==dogs;
+   }
+   
+ 
