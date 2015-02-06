1e54d5bc27b63f30bc09ce9b50bf0b50af2ba578
xp <xp@android028>
2015-02-06 12:40:47 +0100

    DoubleChar Terminado Luis Guillermo Abarca

--- src/codingbatstrings2/DoubleChar_Abarca.java
+++ src/codingbatstrings2/DoubleChar_Abarca.java
@@ -12,12 +12,6 @@ package codingbatstrings2;
 
 /*
 Autor: Luis Guillermo Abarca
-Ejercicio: 
-Given a string, return a string where for every char in the original, there are two chars. 
-
-doubleChar("The") → "TThhee"
-doubleChar("AAbb") → "AAAAbbbb"
-doubleChar("Hi-There") → "HHii--TThheerree"
 */
 public class DoubleChar_Abarca {
     
