03b4224a9534d22296b13112093a6fb6ca625ce5
tian lai zhu <tian lai zhu@tianlaizhu-PC>
2015-02-06 09:27:55 +0100

    Terminado DoubleChar - Pepe Zhu

--- src/codingbatstrings2/DoubleCharPepeZhu.java
+++ src/codingbatstrings2/DoubleCharPepeZhu.java
@@ -10,15 +10,15 @@ package codingbatstrings2;
  * @author tian lai zhu
  */
 public class DoubleCharPepeZhu {
-    public String doubleChar(String str) {
-    //crea el string resultado vacío. 
-String resultado = "";
+public String doubleChar(String str) {
+//Constructor por defecto. El nuevo String toma el valor ""
+String copio = new String(); 
 for(int i = 0; i < str.length();i++){
 // str.charAt (i) devuelve el carácter que está en la posición i.
-
-resultado = resultado + str.charAt(i) + str.charAt(i);
+//Resuelve el problema de copia la letra anterior.
+copio = copio + str.charAt(i) + str.charAt(i);
 }
-return resultado;
+return copio;
 
 }
 }
