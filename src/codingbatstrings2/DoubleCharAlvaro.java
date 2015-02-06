c86b610ff979c82b167edb3c125ed004893580c9
alvaro <alvaro@Alvaro>
2015-02-06 13:05:12 +0100

    06/02/15 DoubleChar_ALVARO//HECHO Y COMENTADO

--- src/codingbatstrings2/DoubleCharAlvaro.java
+++ src/codingbatstrings2/DoubleCharAlvaro.java
@@ -6,9 +6,26 @@
 package codingbatstrings2;
 
 /**
- *
+ * DoubleChar
+ * Tiene que sonseguir dubplicar los char
+ * 
  * @author alvaro
+ * 
  */
 public class DoubleCharAlvaro {
+ public String doubleChar(String str) {
+     //creo una variable
+     String xdos = "";
+     for(int  i = 0; i <= str.length()-1;i++){
+     // variable mas la variable en i    
+     xdos += str.charAt(i);
+     xdos += str.charAt(i);
+     }
+     //muestra el resultado
+     return xdos;
+
+  
+}
+
     
 }
